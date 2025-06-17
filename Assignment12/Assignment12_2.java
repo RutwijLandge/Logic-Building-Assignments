69b758d970abeaddf41642947ced17f630fa1508
RutwijLandge <rutwijlandge2003@gmail.com>
2025-06-17 15:56:29 +0530

    Add files via upload

---
+++ Assignment12/Assignment12_2.java
@@ -0,0 +1,48 @@
+/*
+Enter no of rows :
+4
+Enter no of columns :
+3
+1	2	3	
+1	2	3	
+1	2	3	
+1	2	3
+*/
+import java.util.Scanner;
+
+class Pattern
+{
+    public void Display(int iRow , int iCol)
+    {
+        int i = 0,j = 0;
+             
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                System.out.print(j+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Assignment12_2
+{
+    public static void main(String A [])
+    {
+        int iValue1 = 0,iValue2 = 0;
+
+        Scanner sobj = new Scanner(System.in);
+        Pattern pobj = new Pattern();
+
+        System.out.println("Enter no of rows :");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter no of columns :");
+        iValue2 = sobj.nextInt();
+
+        pobj.Display(iValue1,iValue2);
+
+    }
+}
